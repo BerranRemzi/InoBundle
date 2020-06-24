b8787ce797002b5e9df4aed1f5a398bf0e3dccae
Berran Remzi <11856339+BerranRemzi@users.noreply.github.com>
2020-06-25 00:49:14 +0300

    randomSeed added

--- sim/ArcadeBundleEmulation/Snake/src/emulator/Emulator.h
+++ sim/ArcadeBundleEmulation/Snake/src/emulator/Emulator.h
@@ -1,5 +1,9 @@
 #pragma once
+#include <Windows.h>
+#include <ctime>
 #include "graphics.h"
 
 void updateScreen(uint8_t _input[][8], uint8_t _size);
 int random(int _max);
+void randomSeed(int16_t _input);
+int16_t analogRead(int8_t _pin);
